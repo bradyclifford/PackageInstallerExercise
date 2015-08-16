e63fa23833ff20c6e2db744191e717a264ede15a
Brady Clifford <bradyclifford@gmail.com>
2015-08-16 15:21:03 -0600

    Added Unit Testing Project Skeleton

---
+++ PackageInstallerExercise.Test/ProgramTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PackageInstallerExercise.Test {
+
+  [TestClass]
+  public class ProgramTests {
+
+    [TestMethod]
+    public void TestMethod1() {
+    }
+
+  }
+
+}
