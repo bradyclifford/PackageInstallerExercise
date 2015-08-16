6778ebf852ba8fb155151e965bcb4c67245723c3
Brady Clifford <bradyclifford@gmail.com>
2015-08-16 15:12:38 -0600

    Project Skeleton

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+
+Package Installer Excercise
+Gives the installer a list of packages with dependencies. 
+The installer determines in which order the packages need to be installed.
+
+This exercise is to write the code that will determine the order of install.
+
+*/
+
+namespace PackageInstallerExercise {
+
+  public class Program {
+
+    static void Main(string[] args) {
+
+    }
+
+  }
+
+}
