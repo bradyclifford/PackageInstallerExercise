18941c4dc34da79345727c371b6f11ba9ad8cc3a
Brady Clifford <bradyclifford@gmail.com>
2015-08-17 11:33:41 -0600

    Setup skeleton PackagesDependencyMap class and test files.

---
+++ source/Packages/PackagesDependencyMap.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackageInstallerExercise.Packages {
+
+  /// <summary>
+  /// Container for holding the mapping between package dependencies
+  /// </summary>
+  public class PackagesDependencyMap {
+
+    public void Add(string packageName, string dependencyName) {
+      throw new NotImplementedException();
+    }
+
+    public string[] GetArray() {
+      throw new NotImplementedException();
+    }
+
+  }
+
+}
---
+++ tests/Packages/PackagesDependencyMapTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PackageInstallerExercise.Test {
+
+  [TestClass]
+  public class PackagesDependencyMapTests {
+
+    [TestMethod]
+    public void TestMethod1() {
+    }
+
+  }
+
+}
